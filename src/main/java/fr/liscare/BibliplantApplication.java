ee8bc8644dc0664f23a4896a860b8e6bc02b4b6f
Liscare <liscare2@protonmail.com>
2021-02-23 20:39:01 +0100

    Init a Spring application

---
+++ src/main/java/fr/liscare/BibliplantApplication.java
@@ -0,0 +1,11 @@
+package fr.liscare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliplantApplication {
+    public static void main(String... args) {
+        SpringApplication.run(BibliplantApplication.class, args);
+    }
+}
